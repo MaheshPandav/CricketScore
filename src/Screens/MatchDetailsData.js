c9d2b922a32c7454233d7fa3183e31006fa10448
mahesh <pandavmahesh490@gmail.com>
2023-01-22 12:57:58 +0530

    add over section

--- src/Screens/MatchDetailsData.js
+++ src/Screens/MatchDetailsData.js
@@ -13,6 +13,7 @@ const MatchDetailsData = (props) => {
   const [team, setTeam] = useState(0);
   const [option, setOption] = useState(true);
   const [optionb, setOptionb] = useState(false);
+  const [showWicket, setShowWicket] = useState(true);
 
   console.log(props.data);
   return (
@@ -64,8 +65,15 @@ const MatchDetailsData = (props) => {
           props.data.isLive || props.data.isCompleted ? (
             <div className="Scorecard-page">
               <div className="team-option">
-                <div className="team-a" onClick={() => setOption(!option)} style={{backgroundColor:option ? 'pink' : ''}}>
-                {/* onClick={() => setOption(!option) */}
+                <div
+                  className="team-a"
+                  onClick={() => setOption(!option)}
+                  style={{
+                    backgroundColor: option ? "pink" : "",
+                    borderRadius: 5,
+                  }}
+                >
+                  {/* onClick={() => setOption(!option) */}
                   {props.data.innings[0].battingTeamId && (
                     <p
                       style={{ color: "#fff" }}
@@ -97,7 +105,14 @@ const MatchDetailsData = (props) => {
                     />
                   )}
                 </div>
-                <div className="team-b" onClick={() => setOptionb(!optionb)} style={{backgroundColor: optionb ? 'pink' : ''}}>
+                <div
+                  className="team-b"
+                  onClick={() => setOptionb(!optionb)}
+                  style={{
+                    backgroundColor: optionb ? "pink" : "",
+                    borderRadius: 5,
+                  }}
+                >
                   {props.data.innings[1] &&
                     props.data.innings[0].battingTeamId && (
                       <p
@@ -240,6 +255,169 @@ const MatchDetailsData = (props) => {
                           </div>
                         </div>
                       ))}
+                      <div className="line"></div>
+                      {option && (
+                        <div className="score-info">
+                          <div>
+                            EXTRAS: {props.data.innings[team].totalExtras} (
+                            {props.data.innings[team].wideBalls}Wd,{" "}
+                            {props.data.innings[team].noBalls}NB,{" "}
+                            {props.data.innings[team].legByesRuns}LB,{" "}
+                            {props.data.innings[team].byesRuns}B)
+                          </div>
+                          <div className="run-score">
+                            TOTAL:{" "}
+                            {props.data.innings[team].numberOfWicketsFallen}-
+                            {props.data.innings[team].runsScored} (
+                            {props.data.innings[team].oversBowled} Overs)
+                          </div>
+
+                          <div>
+                            <div className="fall-wickets">
+                              <span>FALL OF WICKETS</span>
+                              <BsChevronDown
+                                size={20}
+                                onClick={() => setShowWicket(!showWicket)}
+                                className="icon"
+                              />
+                            </div>
+                            {!showWicket && (
+                              <div className="wickets-records">
+                                {props.data.innings[team].wickets.length > 0
+                                  ? props.data.innings[team].wickets.map(
+                                      (data, index) => {
+                                        return (
+                                          <div className="wickets-rec">
+                                            <p>
+                                              {data.order}-{data.runs} (
+                                              {data.overBallDisplay}) {""}
+                                              {
+                                                props.player.find(
+                                                  (res) =>
+                                                    res.id === data.playerId
+                                                ).displayName
+                                              }{" "}
+                                            </p>
+                                          </div>
+                                        );
+                                      }
+                                    )
+                                  : null}
+                              </div>
+                            )}
+                          </div>
+                          <div className="score-head">
+                            <div className="score-header">
+                              <FaBaseballBall size={20} className="icon" />
+                              <h3 className="batting">Bowling</h3>
+                            </div>
+                            <h3 className="data">O</h3>
+                            <h3 className="data">M</h3>
+                            <h3 className="data">R</h3>
+                            <h3 className="data">W</h3>
+                            <h3 className="strike-rate">Eco</h3>
+                          </div>
+                          {props.data.innings[team].bowlers.map(
+                            (res, index) => (
+                              <div className="player-image">
+                                <div className="player-name">
+                                  <img
+                                    src={
+                                      props.player.find(
+                                        (data) => data.id === res.playerId
+                                      ).imageUrl
+                                        ? props.player.find(
+                                            (data) => data.id === res.playerId
+                                          ).imageUrl
+                                        : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlrNp5xmRAAnjGHuHgHWBjdBGwTWF2RFHnQQ&usqp=CAU"
+                                    }
+                                    alt="profile"
+                                    className="cursor-pointer"
+                                  />
+                                  <p className="player">
+                                    {
+                                      props.player.find(
+                                        (data) => data.id === res.playerId
+                                      ).displayName
+                                    }
+                                    {res.isOnStrike ? (
+                                      <FaBaseballBall
+                                        size={18}
+                                        color={"green"}
+                                        style={{ marginLeft: 10 }}
+                                      />
+                                    ) : null}
+                                  </p>
+                                </div>
+                                <div className="scored">
+                                  {res.oversBowled ? (
+                                    res.isOnStrike ? (
+                                      res.ballsBowled ? (
+                                        <p className="runs">
+                                          {res.oversBowled}.{res.ballsBowled}
+                                        </p>
+                                      ) : (
+                                        <p className="runs">
+                                          {res.oversBowled}*
+                                        </p>
+                                      )
+                                    ) : (
+                                      <p className="runs">{res.oversBowled}</p>
+                                    )
+                                  ) : res.oversBowled === 0 ? (
+                                    res.isOnStrike ? (
+                                      res.ballsBowled ? (
+                                        <p className="runs">
+                                          {res.oversBowled}.{res.ballsBowled}
+                                        </p>
+                                      ) : (
+                                        <p className="runs">
+                                          {res.oversBowled}*
+                                        </p>
+                                      )
+                                    ) : (
+                                      <p className="runs">{res.oversBowled}*</p>
+                                    )
+                                  ) : (
+                                    <p className="runs">-</p>
+                                  )}
+                                  <p className="runs">
+                                    {res.maidensBowled ||
+                                    res.maidensBowled === 0 ? (
+                                      res.maidensBowled
+                                    ) : (
+                                      <p className="runs">-</p>
+                                    )}
+                                  </p>
+                                  <p className="runs">
+                                    {res.runsConceded ||
+                                    res.runsConceded === 0 ? (
+                                      res.runsConceded
+                                    ) : (
+                                      <p className="runs">-</p>
+                                    )}
+                                  </p>
+                                  <p className="runs">
+                                    {res.wicketsTaken ||
+                                    res.wicketsTaken === 0 ? (
+                                      res.wicketsTaken
+                                    ) : (
+                                      <p className="runs">-</p>
+                                    )}
+                                  </p>
+                                  <p className="runs">
+                                    {res.economy || res.economy === 0 ? (
+                                      res.economy
+                                    ) : (
+                                      <p className="runs">-</p>
+                                    )}
+                                  </p>
+                                </div>
+                              </div>
+                            )
+                          )}
+                        </div>
+                      )}
                     </div>
                   )}
                 </div>
@@ -341,11 +519,163 @@ const MatchDetailsData = (props) => {
                       ))}
                     </div>
                   )}
+                  <div className="line"></div>
+                  {optionb && (
+                    <div className="score-info">
+                      <div>
+                        EXTRAS: {props.data.innings[1].totalExtras} (
+                        {props.data.innings[1].wideBalls}Wd,{" "}
+                        {props.data.innings[1].noBalls}NB,{" "}
+                        {props.data.innings[1].legByesRuns}LB,{" "}
+                        {props.data.innings[1].byesRuns}B)
+                      </div>
+                      <div className="run-score">
+                        TOTAL: {props.data.innings[1].numberOfWicketsFallen}-
+                        {props.data.innings[1].runsScored} (
+                        {props.data.innings[1].oversBowled} Overs)
+                      </div>
+
+                      <div>
+                        <div className="fall-wickets">
+                          <span>FALL OF WICKETS</span>
+                          <BsChevronDown
+                            size={20}
+                            onClick={() => setShowWicket(!showWicket)}
+                            className="icon"
+                          />
+                        </div>
+                        {!showWicket && (
+                          <div className="wickets-records">
+                            {props.data.innings[1].wickets.length > 0
+                              ? props.data.innings[1].wickets.map(
+                                  (data, index) => {
+                                    return (
+                                      <div className="wickets-rec">
+                                        <p>
+                                          {data.order}-{data.runs} (
+                                          {data.overBallDisplay}) {""}
+                                          {
+                                            props.player.find(
+                                              (res) => res.id === data.playerId
+                                            ).displayName
+                                          }{" "}
+                                        </p>
+                                      </div>
+                                    );
+                                  }
+                                )
+                              : null}
+                          </div>
+                        )}
+                      </div>
+                      <div className="score-head">
+                        <div className="score-header">
+                          <FaBaseballBall size={20} className="icon" />
+                          <h3 className="batting">Bowling</h3>
+                        </div>
+                        <h3 className="data">O</h3>
+                        <h3 className="data">M</h3>
+                        <h3 className="data">R</h3>
+                        <h3 className="data">W</h3>
+                        <h3 className="strike-rate">Eco</h3>
+                      </div>
+                      {props.data.innings[1].bowlers.map((res, index) => (
+                        <div className="player-image">
+                          <div className="player-name">
+                            <img
+                              src={
+                                props.player.find(
+                                  (data) => data.id === res.playerId
+                                ).imageUrl
+                                  ? props.player.find(
+                                      (data) => data.id === res.playerId
+                                    ).imageUrl
+                                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlrNp5xmRAAnjGHuHgHWBjdBGwTWF2RFHnQQ&usqp=CAU"
+                              }
+                              alt="profile"
+                              className="cursor-pointer"
+                            />
+                            <p className="player">
+                              {
+                                props.player.find(
+                                  (data) => data.id === res.playerId
+                                ).displayName
+                              }
+                              {res.isOnStrike ? (
+                                <FaBaseballBall
+                                  size={18}
+                                  color={"green"}
+                                  style={{ marginLeft: 10 }}
+                                />
+                              ) : null}
+                            </p>
+                          </div>
+                          <div className="scored">
+                            {res.oversBowled ? (
+                              res.isOnStrike ? (
+                                res.ballsBowled ? (
+                                  <p className="runs">
+                                    {res.oversBowled}.{res.ballsBowled}
+                                  </p>
+                                ) : (
+                                  <p className="runs">{res.oversBowled}*</p>
+                                )
+                              ) : (
+                                <p className="runs">{res.oversBowled}</p>
+                              )
+                            ) : res.oversBowled === 0 ? (
+                              res.isOnStrike ? (
+                                res.ballsBowled ? (
+                                  <p className="runs">
+                                    {res.oversBowled}.{res.ballsBowled}
+                                  </p>
+                                ) : (
+                                  <p className="runs">{res.oversBowled}*</p>
+                                )
+                              ) : (
+                                <p className="runs">{res.oversBowled}*</p>
+                              )
+                            ) : (
+                              <p className="runs">-</p>
+                            )}
+                            <p className="runs">
+                              {res.maidensBowled || res.maidensBowled === 0 ? (
+                                res.maidensBowled
+                              ) : (
+                                <p className="runs">-</p>
+                              )}
+                            </p>
+                            <p className="runs">
+                              {res.runsConceded || res.runsConceded === 0 ? (
+                                res.runsConceded
+                              ) : (
+                                <p className="runs">-</p>
+                              )}
+                            </p>
+                            <p className="runs">
+                              {res.wicketsTaken || res.wicketsTaken === 0 ? (
+                                res.wicketsTaken
+                              ) : (
+                                <p className="runs">-</p>
+                              )}
+                            </p>
+                            <p className="runs">
+                              {res.economy || res.economy === 0 ? (
+                                res.economy
+                              ) : (
+                                <p className="runs">-</p>
+                              )}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
           ) : (
-            ""
+            <p>March is Not started</p>
           )
         ) : props.tab === "Commentry" ? (
           <div className="Commentry-page">
